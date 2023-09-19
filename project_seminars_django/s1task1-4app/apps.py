669c82217720d2e2ea01fffb92cbd346e441d43b
MarSerMer <marinakms@yandex.ru>
2023-09-19 22:27:11 +0300

    Lecture and seminar 1

---
+++ project_seminars_django/s1task1-4app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SemsappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 's1task1-4app'
---
+++ project_seminars_django/s1task5app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import random
+from django.http import HttpResponse
+import logging
+
+# Create your views here.
+# Создайте новое приложение. Подключите его к проекту.
+# В приложении должно быть три простых представления, возвращающих HTTP ответ:
+# Орёл или решка
+# Значение одной из шести граней игрального кубика
+# Случайное число от 0 до 100
+
+logger = logging.getLogger(__name__)
+
+
+def one(request):
+    logger.info('One page started')
+    answer = ['Орел', 'Решка']
+    i = random.randint(0, 1)
+    return HttpResponse(f'{answer[i]}')
+
+
+def two(request):
+    logger.info('Two page started')
+    answer = random.randint(1, 6)
+    return HttpResponse(f'{answer}')
+
+
+def three(request):
+    logger.info('Three page started')
+    answer = random.randint(0, 100)
+    return HttpResponse(f'{answer}')
